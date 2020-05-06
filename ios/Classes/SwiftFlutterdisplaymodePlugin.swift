f940a4fc277ce721ce42f65a23976a5935bee24c
masewo <masewo@gmx.net>
2020-05-06 22:45:54 +0200

    try to fix ios build error

--- ios/Classes/SwiftFlutterdisplaymodePlugin.swift
+++ ios/Classes/SwiftFlutterdisplaymodePlugin.swift
@@ -3,7 +3,7 @@ import UIKit
 
 public class SwiftFlutterdisplaymodePlugin: NSObject, FlutterPlugin {
   public static func register(with registrar: FlutterPluginRegistrar) {
-    let channel = FlutterMethodChannel(name: "flutter_displaymode", binaryMessenger: registrar.messenger())
+    let channel = FlutterMethodChannel(name: "flutterdisplaymode", binaryMessenger: registrar.messenger())
     let instance = SwiftFlutterdisplaymodePlugin()
     registrar.addMethodCallDelegate(instance, channel: channel)
   }
