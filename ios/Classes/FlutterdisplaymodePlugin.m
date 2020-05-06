8c259a80a254553971acb38f389daa8cb9161f59
masewo <masewo@gmx.net>
2020-05-06 22:56:49 +0200

    try to fix ios build error by throwing away swift support

--- ios/Classes/FlutterdisplaymodePlugin.m
+++ ios/Classes/FlutterdisplaymodePlugin.m
@@ -1,15 +1,20 @@
 #import "FlutterdisplaymodePlugin.h"
-#if __has_include(<flutterdisplaymode/flutterdisplaymode-Swift.h>)
-#import <flutterdisplaymode/flutterdisplaymode-Swift.h>
-#else
-// Support project import fallback if the generated compatibility header
-// is not copied when this plugin is created as a library.
-// https://forums.swift.org/t/swift-static-libraries-dont-copy-generated-objective-c-header/19816
-#import "flutterdisplaymode-Swift.h"
-#endif
 
 @implementation FlutterdisplaymodePlugin
 + (void)registerWithRegistrar:(NSObject<FlutterPluginRegistrar>*)registrar {
-  [SwiftFlutterdisplaymodePlugin registerWithRegistrar:registrar];
+  FlutterMethodChannel* channel = [FlutterMethodChannel
+      methodChannelWithName:@"flutterdisplaymode"
+            binaryMessenger:[registrar messenger]];
+  FlutterdisplaymodePlugin* instance = [[FlutterdisplaymodePlugin alloc] init];
+  [registrar addMethodCallDelegate:instance channel:channel];
 }
+
+- (void)handleMethodCall:(FlutterMethodCall*)call result:(FlutterResult)result {
+  if ([@"getPlatformVersion" isEqualToString:call.method]) {
+    result([@"iOS " stringByAppendingString:[[UIDevice currentDevice] systemVersion]]);
+  } else {
+    result(FlutterMethodNotImplemented);
+  }
+}
+
 @end
